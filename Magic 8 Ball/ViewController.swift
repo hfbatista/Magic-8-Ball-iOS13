96511d6b8801a504ea13b94fd9454a0256a3133b
Henrique Batista <hfbatista@hotmail.com>
2020-09-16 14:55:56 -0300

    Implementar lógica

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,8 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    @IBOutlet weak var ballImageView: UIImageView!
+    @IBOutlet weak var askButton: UIButton!
 
 
-
+    @IBAction func ballButtonPressed(_ sender: UIButton) {
+        self.askButton.setTitle("", for: .normal)
+        self.ballImageView.image = ballArray[Int.random(in: 0..<ballArray.count)]
+    }
+    
 }
 
